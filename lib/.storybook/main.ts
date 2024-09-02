ac347a16ad016ec096f6d3fe265059e6869c3696
Monika Wiercinska <monika.wiercinska@yld.io>
2024-09-02 14:12:53 +0100

    chore(lib): add theme mode control to storybook

--- lib/.storybook/main.ts
+++ lib/.storybook/main.ts
@@ -2,7 +2,7 @@ import type { StorybookConfig } from '@storybook/react-webpack5';
 
 const config: StorybookConfig = {
   stories: ['../__stories__/**/*.stories.@(js|jsx|mjs|ts|tsx)'],
-  addons: ['@storybook/addon-webpack5-compiler-swc'],
+  addons: ['@storybook/addon-webpack5-compiler-swc', '@storybook/addon-controls'],
   framework: {
     name: '@storybook/react-webpack5',
     options: {},
