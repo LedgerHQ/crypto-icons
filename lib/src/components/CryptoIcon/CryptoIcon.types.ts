3561bb67f91c53120d604ac372625b3f29964487
Marco Toniut <marco.toniut-ext@ledger.fr>
2025-06-06 18:21:29 +0100

    feat: add '20px' option for CryptoIcon, and some storybook improvement

--- lib/src/components/CryptoIcon/CryptoIcon.types.ts
+++ lib/src/components/CryptoIcon/CryptoIcon.types.ts
@@ -1,10 +1,13 @@
 import { ThemeNames } from '@ledgerhq/ui-shared';
 import { Currency } from '@ledgerhq/wallet-api-client';
 
+/**
+ * @reference [Figma](https://www.figma.com/design/zSkvGGiqcnhywp2l3HTHxA/1.Symbol-Library?node-id=6-3803&t=W0J5MFxuaIyDedJU-0)
+ */
 export type CryptoIconProps = {
   ledgerId: Currency['id'];
   ticker: Currency['ticker'];
   theme?: ThemeNames;
-  size?: '16px' | '24px' | '32px' | '40px' | '48px' | '56px';
+  size?: '16px' | '20px' | '24px' | '32px' | '40px' | '48px' | '56px';
   network?: Currency['id'];
 };
